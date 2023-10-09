8296de0a230f07ce131e41b6e49cfd2ea078d33d
Thiago Regueira <thiago.regueira@yahoo.com.br>
2023-10-09 14:00:24 -0300

    criação do modelo Produto

---
+++ src/main/java/br/com/api/produtos/model/Produto.java
@@ -0,0 +1,65 @@
+package br.com.api.produtos.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+    // #region Atributtos
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String nome;
+    private Integer quantidade;
+    private Double valor;
+    private String observacao;
+    // #endregion
+
+    // #region Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+    // #endregion
+
+}
